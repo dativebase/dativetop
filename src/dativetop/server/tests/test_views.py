ff410bffbcc04d0e8b1a4aa478679ad2515aa250
Joel Dunham <jrwdunham@gmail.com>
2021-03-30 21:18:39 -0700

    Add /dative_app endpoint
    - GET request returns *the* Dative App
    - PUT request allows for the URL to be modified
    - Tests

--- src/dativetop/server/tests/test_views.py
+++ src/dativetop/server/tests/test_views.py
@@ -42,7 +42,7 @@ class ViewsTests(unittest.TestCase):
             json_body={'url': new_new_url}, method='POST')
         response = v.old_service(request)
         self.assertEqual(
-            'The old_service only recognizes GET and PUT requests.',
+            'The old_service endpoint only recognizes GET and PUT requests.',
             response['error'])
         v.old_service(testing.DummyRequest(json_body={'url': new_new_url},
                                            method='PUT'))
@@ -50,15 +50,42 @@ class ViewsTests(unittest.TestCase):
         self.assertEqual(3, len(old_service_rows))
         self.assertEqual(1, len(set([os.history_id for os in old_service_rows])))
 
-    def test_old_service_url_validation(self):
+    def test_dative_app_api(self):
+        import dativetopserver.views as v
+        import dativetopserver.models as m
+        self.assertEqual([], self.session.query(m.DativeApp).all())
+        request = testing.DummyRequest(method='GET')
+        response = v.dative_app(request)
+        self.assertEqual(1, len(self.session.query(m.DativeApp).all()))
+        self.assertEqual(m.DEFAULT_DATIVE_APP_URL, response['url'])
+        new_url = 'http://localhost:1111'
+        request = testing.DummyRequest(
+            json_body={'url': new_url}, method='PUT')
+        response = v.dative_app(request)
+        self.assertEqual(2, len(self.session.query(m.DativeApp).all()))
+        self.assertEqual(new_url, response['url'])
+        new_new_url = 'http://localhost:2222'
+        request = testing.DummyRequest(
+            json_body={'url': new_new_url}, method='POST')
+        response = v.dative_app(request)
+        self.assertEqual(
+            'The dative_app endpoint only recognizes GET and PUT requests.',
+            response['error'])
+        v.dative_app(testing.DummyRequest(json_body={'url': new_new_url},
+                                           method='PUT'))
+        dative_app_rows = self.session.query(m.DativeApp).all()
+        self.assertEqual(3, len(dative_app_rows))
+        self.assertEqual(1, len(set([os.history_id for os in dative_app_rows])))
+
+    def test_local_url_validation(self):
         import dativetopserver.views as v
         self.assertEqual('scheme is not http',
-                         v.validate_old_service_url('https://localhost:1111'))
+                         v.validate_local_url('https://localhost:1111'))
         self.assertEqual('no port',
-                         v.validate_old_service_url('http://localhost'))
+                         v.validate_local_url('http://localhost'))
         self.assertIn('hostname invalid',
-                      v.validate_old_service_url('http://myhost:1111'))
+                      v.validate_local_url('http://myhost:1111'))
         self.assertEqual('path is not empty',
-                      v.validate_old_service_url('http://127.0.0.1:1111/foo'))
-        self.assertIsNone(v.validate_old_service_url('http://127.0.0.1:1111/'))
-        self.assertIsNone(v.validate_old_service_url('http://localhost:5678'))
+                      v.validate_local_url('http://127.0.0.1:1111/foo'))
+        self.assertIsNone(v.validate_local_url('http://127.0.0.1:1111/'))
+        self.assertIsNone(v.validate_local_url('http://localhost:5678'))
